42cefbbec5adff16333c9f79d2ddc0c27b45bf29
Quinn Weaver <quinn.weaver@gmail.com>
2025-02-02 02:41:22 -0800

    Make the output separator global
    (since both lx() and parse() will need it).

--- lisp.bash
+++ lisp.bash
@@ -1,5 +1,14 @@
 #!/opt/homebrew/bin/bash
 
+# Define an output token separator that won't be used in the
+# input Lisp.    
+# ASCII has a chacter for just such a purpose:
+# local SEP=$'\x1F'
+# But it's not printable, which makes print-style debugging hard.
+# So just use newline. That way we don't have to change IFS, either.
+SEP=$'\n'    
+
+
 function debug {
     local DBGLVL="${DEBUG_LEVEL_FOR_LISP_DOT_BASH:-0}"
     ### echo "Debug level: $DBGLVL" >/dev/stderr
@@ -65,15 +74,6 @@ function lx {
     debug "Code with whitespace normalized:" \
           "$CODE"
 
-    # Define an output token separator that won't be used in the
-    # input Lisp.    
-    # ASCII has a chacter for just such a purpose:
-    # local SEP=$'\x1F'
-    # But it's not printable, which makes print-style debugging hard.
-    # So just use newline.
-    # local SEP=$'\n'
-    local SEP='|'
-    
     # Replace each resulting space with the separator character.
     CODE="${CODE// /${SEP}}"
 
@@ -88,7 +88,6 @@ $CODE"
 
     echo "$CODE"
 }
-
 
 
 main "$@"
