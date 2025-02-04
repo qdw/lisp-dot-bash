a13b0a2197173c453b36b9079c400959c5a26f1e
Quinn Weaver <quinn.weaver@gmail.com>
2025-02-03 18:59:45 -0800

    Add shell functions to run the program and the list-op test suite

--- functions.bash
+++ functions.bash
@@ -1,7 +1,8 @@
 function l {
-    cd ~/src/lisp-dot-bash && DEBUG_LEVEL_FOR_LISP_DOT_BASH=2 ./list-operations.bash
+    cd ~/src/lisp-dot-bash \
+        && DEBUG_LEVEL_FOR_LISP_DOT_BASH=2 ./list-operations.bash
 }
 
 function t {
-    ~/src/lisp-dot-bash/t
-}
+    cd ~/src/lisp-dot-bash \
+       && DEBUG_LEVEL_FOR_LISP_DOT_BASH=2 ~/src/lisp-dot-bash/lisp.bash "(+  1 2 (*   3 (/ 88 4 )))"
