00d77ac9fc0f1c95292986cc7b74e2402b1636d8
Quinn Weaver <quinn.weaver@gmail.com>
2025-02-04 20:14:32 -0800

    Implement more Scheme standard functions (and rename others)

--- list-operations.bash
+++ list-operations.bash
@@ -2,6 +2,34 @@
 
 DEBUG_LEVEL="$DEBUG_LEVEL_FOR_LISP_DOT_BASH"
 
+function add {
+    local ACC=0
+    for ARG in "$@"; do
+        ((ACC = ACC + ARG))
+    done
+    echo "$ACC"
+}
+
+# Operator overloading in Bash has limitations.
+# You can define a function named +
+# but you can't define a function named >
+# So, instead, we'll map all Lisp operators
+# to function calls, like this:
+#
+# lisp op   bash function
+# +         add
+# >         greater than
+# ...
+# function + {
+#     add "$@"
+# }
+
+function apply {
+    local FUNC="$1"
+    shift
+    $FUNC "$@"
+}
+
 function length {
     local -a LIST=("$@")
     echo ${#LIST[@]}
@@ -140,33 +168,32 @@ function _alt_pop {
 function run-unit-tests {
     #### Fixtures
     
-    local FLAT_LIST=("+ 1 2 97")
-    local CONSTRUCTED_FLAT_LIST="$(list + 2 4 200)"
+    local -a FLAT_LIST=("+" "1" "2" "97")
+    local -a CONSTRUCTED_FLAT_LIST="$(list + 2 4 200)"
     
-    #### Working tests
+    #### Working tests (denoted by commenting out with '## ')
 
     debug "literal flat list:" ${FLAT_LIST[@]}
-    
     debug "constructed flat list:" ${CONSTRUCTED_FLAT_LIST[@]}
-    
+
     local LEN=$(length ${FLAT_LIST[@]})
     debug "list length: $LEN"
 
     local CAR="$(car ${FLAT_LIST[@]})"
     debug "car: $CAR"
-    
+
     local ALT_CAR="$(alt-car ${FLAT_LIST[@]})"
     debug "alt-car: $CAR"
-    
+
     local -a CDR="$(cdr ${FLAT_LIST[@]})"
     debug "cdr: $CDR"
-    
+
     local -a ALT_CDR="$(alt-cdr ${FLAT_LIST[@]})"
     debug "alt-cdr: ${ALT_CDR[@]}"
-    
+
     local LAST_ELEM="$(last-elem-of ${FLAT_LIST[@]})"
     debug "last element: $LAST_ELEM"
-    
+
     local ALT_LAST_ELEM="$(alt-last-elem-of ${FLAT_LIST[@]})"
     debug "alt last element: $ALT_LAST_ELEM"
 
@@ -179,22 +206,74 @@ function run-unit-tests {
         local NEW_LIST_NAME="$3"
         eval "IFS=$'\x1F' read -r $ELEM_NAME $NEW_LIST_NAME <<< \"\$(_pop \${$LIST_NAME[@]})\""
     }
-
+    
+    function push {
+        local ELEM="$1"
+        shift
+        
+        list "$@" $ELEM
+    }
+    
     pop FLAT_LIST  POPPED_ELEM  POPPED_LIST
     debug "popped last element: $POPPED_ELEM"
     debug "list after popping: $POPPED_LIST"
     
-    function alt_pop {
+    function pop-by-reference {
         local LIST_NAME="$1"
         local ELEM_NAME="$2"
         local NEW_LIST_NAME="$3"
         eval "IFS=$'\x1F' read -r $ELEM_NAME $NEW_LIST_NAME <<< \"\$(_alt_pop \${$LIST_NAME[@]})\""
     }
 
-    alt_pop FLAT_LIST  ALT_POPPED_ELEM  ALT_POPPED_LIST
-    debug "alt-popped last element: $ALT_POPPED_ELEM"
-    debug "list after alt-popping: $ALT_POPPED_LIST"
-}
+    pop-by-reference FLAT_LIST  ALT_POPPED_ELEM  ALT_POPPED_LIST
+    debug "pop-by-reference set ALT_POPPED_ELEM to this value: $ALT_POPPED_ELEM"
+    debug "pop-by-reference set ALT_POPPED_LIST to this value: ${ALT_POPPED_LIST[@]}"
+    
+    local -a PUSH_RETVAL=("$(push "f" "${FLAT_LIST[@]}")")
+    debug "pushed-to list: ${PUSH_RETVAL[@]}"
+    
+    SUM="$(add 1 2 3 4)"
+    debug "add 1 2 3 4 -> $SUM"
+    
+    local -a ARR=("1" "2" "3" "4")
+    ARRSUM=$(add ${ARR[@]})
+    debug "add with array arg -> $ARRSUM"
+    
+    APSUM="$(apply add 1 2 3 4)"
+    debug "apply add 1 2 3 4 -> $APSUM"
+}    
+
+
+
+# Standard operations defined in Scheme
+
+
+
+# >op.gt,
+# abs
+# append
+# apply
+# begin
+# car
+# cdr
+# cons
+# eq?
+# expt
+# equal?
+# length
+# list
+# list?
+# map
+# max
+# min
+# not
+# null?
+# number?
+# print
+# procedure?
+# round
+# symbol?
+
 
 # Run the main function only if this script was executed directly;
 # don't run it if this script was source'd by a different Bash script.
