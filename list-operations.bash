048b8d341d95763cb9328bacd785f9b6609bfd50
Quinn Weaver <quinn.weaver@gmail.com>
2025-02-05 18:20:59 -0800

    Abstract all arithmetic operations to use a single function

--- list-operations.bash
+++ list-operations.bash
@@ -3,8 +3,6 @@
 DEBUG_LEVEL="$DEBUG_LEVEL_FOR_LISP_DOT_BASH"
 
 function arith {
-    set -x
-    
     local OP="$1"
     shift
  
@@ -12,26 +10,6 @@ function arith {
     shift
     for ARG in "$@"; do
         ((ACC=ACC "$OP" ARG))
-    done
-    echo "$ACC"
-    
-    set -x
-}
-
-function div {
-    local ACC="$1"
-    shift
-    debug "ACC: $ACC, rest: $@"
-    for ARG in "$@"; do
-        ((ACC = ACC / $ARG))
-    done
-    echo "$ACC"
-}
-
-function add {
-    local ACC=0
-    for ARG in "$@"; do
-        ((ACC = ACC + ARG))
     done
     echo "$ACC"
 }
@@ -258,24 +236,11 @@ function run-unit-tests {
     local -a PUSH_RETVAL=("$(push "f" "${FLAT_LIST[@]}")")
     debug "pushed-to list: ${PUSH_RETVAL[@]}"
     
-    SUM="$(add 1 2 3 4)"
-    debug "add 1 2 3 4 -> $SUM"
+    SUM="$(arith '+' 1 2 3 4)"
+    debug "arith '+' 1 2 3 4 -> $SUM"
     
-    local -a ARR=("1" "2" "3" "4")
-    ARRSUM=$(add ${ARR[@]})
-    debug "add with array arg -> $ARRSUM"
-    
-    local APSUM="$(apply add 1 2 3 4)"
-    debug "apply add 1 2 3 4 -> $APSUM"
-    
-    local DIVIDEND=$(div 64 2 2 4)
-    debug "div 64 2 2 4 -> $DIVIDEND"
-    
-    local ABSTRACTED_DIVIDEND="$(arith '/' 64 2 2 4)"
-    debug "arith '/' 64 2 2 4 -> $ABSTRACTED_DIVIDEND"
-    
-    local ABSTRACTED_SUM="$(arith '+' 1 2 3 4)"
-    debug "arith '+' 1 2 3 4 -> $ABSTRACTED_SUM"
+    local DIVIDEND="$(arith '/' 64 2 2 4)"
+    debug "arith '/' 64 2 2 4 -> $DIVIDEND"
 }
 
 
