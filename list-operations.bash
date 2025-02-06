63584f7adb6dbbb57ff0c395f34a5b9b8cc6a3c0
Quinn Weaver <quinn.weaver@gmail.com>
2025-02-06 03:52:27 -0800

    list_ops implementations: keep best, del rest. Add tap tests!

--- list-operations.bash
+++ list-operations.bash
@@ -1,6 +1,14 @@
 #!/opt/homebrew/bin/bash
 
+source "$(dirname "$0")/tap.bash"
+
 DEBUG_LEVEL="$DEBUG_LEVEL_FOR_LISP_DOT_BASH"
+
+function abs {
+    local NUM="$1"
+    NUM="${NUM//-/}"
+    echo "$NUM"
+}
 
 function arith {
     local OP="$1"
@@ -14,19 +22,13 @@ function arith {
     echo "$ACC"
 }
 
-# Operator overloading in Bash has limitations.
-# You can define a function named +
-# but you can't define a function named >
-# So, instead, we'll map all Lisp operators
-# to function calls, like this:
-#
-# lisp op   bash function
-# +         add
-# >         greater than
-# ...
-# function + {
-#     add "$@"
-# }
+function gt {
+    if [[ "$1" -gt "$2" ]]; then
+        echo "'t"
+    else
+        echo "nil"
+    fi
+}
 
 function apply {
     local FUNC="$1"
@@ -40,6 +42,10 @@ function length {
 }
 
 function debug {
+    if [[ "$DEBUG_LEVEL" = "0" ]] || [[ "$DEBUG_LEVEL" = ""  ]]; then
+        return
+    fi
+
     local CALLER="${FUNCNAME[1]}"
     local STACK_DEPTH=$(length ${FUNCNAME[@]})
     local STACK_DEPTH_OF_CALLER=$((STACK_DEPTH - 2))
@@ -76,42 +82,16 @@ function list {
 #     done
 # }
 
-function debug-car {
-    local -a LIST=("$@")
-    debug "input: " ${LIST[@]}
-    local RETVAL="${LIST[0]}"
-    debug "output: $RETVAL"
-    echo "$RETVAL"
-}
-
 function car {
-    local -a LIST=("$@")
-    echo "${LIST[0]}"
-}
-
-function alt-car {
     echo "$1"
 }
 
-function debug-cdr {
-    local -a LIST=("$@")
-    debug "input: ${LIST[@]}"
-    local LENGTH=${#LIST[@]}
-    debug "length: $LENGTH"
-    echo ${LIST[@]:1:LENGTH}
-}
-
 function cdr {
-    local -a LIST=("$@")
-    local LENGTH=${#LIST[@]}
-    echo ${LIST[@]:1:LENGTH}
-}
-
-function alt-cdr {
     shift
+
     echo "$@"
 }
-    
+
 # I can't figure out how to implement pop in Bash
 # without resorting to eval, because Bash allows
 # neither array pass-by-reference nor returning
@@ -135,11 +115,6 @@ function alt-cdr {
 # but I like the evil one better. Who wants to remember
 # to call two functions?
 function last-elem-of {
-    local -a LIST=("$@")
-    echo "${LIST[-1]}"
-}
-
-function alt-last-elem-of {
     echo ${!#}
 }
 
@@ -152,16 +127,6 @@ function list-sans-last-elem {
 
 function _pop {
     local -a LIST=("$@")
-
-    local LAST="$(last-elem-of ${LIST[@]})"
-    local -a REST="$(list-sans-last-elem ${LIST[@]})"
-    
-    OFS=$'\x1F'
-    printf "${LAST}${OFS}${REST}"
-}
-
-function _alt_pop {
-    local -a LIST=("$@")
     local LAST_ELEM="${LIST[-1]}"
     unset LIST[-1]
     
@@ -170,6 +135,8 @@ function _alt_pop {
 }
 
 function run-unit-tests {
+    plan 10
+
     #### Fixtures
     
     local -a FLAT_LIST=("+" "1" "2" "97")
@@ -178,84 +145,92 @@ function run-unit-tests {
     #### Working tests (denoted by commenting out with '## ')
 
     debug "literal flat list:" ${FLAT_LIST[@]}
+    is  "${FLAT_LIST[*]}"  "+ 1 2 97"  "literal list"
+
     debug "constructed flat list:" ${CONSTRUCTED_FLAT_LIST[@]}
+    is  "${CONSTRUCTED_FLAT_LIST}"  "+ 2 4 200"  "op 'list'"    
 
     local LEN=$(length ${FLAT_LIST[@]})
     debug "list length: $LEN"
+    is  "$LEN"  4  "op 'length'"
 
     local CAR="$(car ${FLAT_LIST[@]})"
     debug "car: $CAR"
-
-    local ALT_CAR="$(alt-car ${FLAT_LIST[@]})"
-    debug "alt-car: $CAR"
+    is  "$CAR" "+" "op 'car'"
 
     local -a CDR="$(cdr ${FLAT_LIST[@]})"
     debug "cdr: $CDR"
-
-    local -a ALT_CDR="$(alt-cdr ${FLAT_LIST[@]})"
-    debug "alt-cdr: ${ALT_CDR[@]}"
+    is  "$CDR"  "1 2 97"  "op 'cdr'"
 
     local LAST_ELEM="$(last-elem-of ${FLAT_LIST[@]})"
     debug "last element: $LAST_ELEM"
 
-    local ALT_LAST_ELEM="$(alt-last-elem-of ${FLAT_LIST[@]})"
-    debug "alt last element: $ALT_LAST_ELEM"
-
     local -a TRUNCATED_LIST=$(list-sans-last-elem ${FLAT_LIST[@]})
     debug "list after removing last element: ${TRUNCATED_LIST[@]}"
 
-    function pop {
+    function push {
+        local ELEM="$1"
+        shift
+        
+        echo "$@" "$ELEM"
+    }
+    
+    function pop-by-ref {
         local LIST_NAME="$1"
         local ELEM_NAME="$2"
         local NEW_LIST_NAME="$3"
         eval "IFS=$'\x1F' read -r $ELEM_NAME $NEW_LIST_NAME <<< \"\$(_pop \${$LIST_NAME[@]})\""
     }
     
-    function push {
-        local ELEM="$1"
-        shift
-        
-        list "$@" $ELEM
-    }
-    
-    pop FLAT_LIST  POPPED_ELEM  POPPED_LIST
+    local POPPED_ELEM
+    local -a POPPED_LIST
+    pop-by-ref FLAT_LIST  POPPED_ELEM  POPPED_LIST
     debug "popped last element: $POPPED_ELEM"
-    debug "list after popping: $POPPED_LIST"
-    
-    function pop-by-reference {
-        local LIST_NAME="$1"
-        local ELEM_NAME="$2"
-        local NEW_LIST_NAME="$3"
-        eval "IFS=$'\x1F' read -r $ELEM_NAME $NEW_LIST_NAME <<< \"\$(_alt_pop \${$LIST_NAME[@]})\""
-    }
+    debug "list after popping: ${POPPED_LIST[@]}"
 
-    pop-by-reference FLAT_LIST  ALT_POPPED_ELEM  ALT_POPPED_LIST
-    debug "pop-by-reference set ALT_POPPED_ELEM to this value: $ALT_POPPED_ELEM"
-    debug "pop-by-reference set ALT_POPPED_LIST to this value: ${ALT_POPPED_LIST[@]}"
-    
     local -a PUSH_RETVAL=("$(push "f" "${FLAT_LIST[@]}")")
     debug "pushed-to list: ${PUSH_RETVAL[@]}"
     
-    SUM="$(arith '+' 1 2 3 4)"
+    SUM="$(arith '+' 1 2 3 4)" # 10
     debug "arith '+' 1 2 3 4 -> $SUM"
     
-    local DIVIDEND="$(arith '/' 64 2 2 4)"
+    local DIVIDEND="$(arith '/' 64 2 2 4)" # 4
     debug "arith '/' 64 2 2 4 -> $DIVIDEND"
-}
+    
+    local PRODUCT="$(arith '*' 2 8 4)" # 64
+    debug "arith '*' 2 8 4 -> $PRODUCT"
+    
+    local MINOR="$(arith '-' 32 16 6)" # 10
+    debug "arith '-' 32 16 6 -> $MINOR"
+    
+    local ABS_POS="$(abs 100)"
+    debug "abs 100 -> $ABS_POS" # 100
+    is  "$ABS_POS"  "100"  "op 'abs' with positive arg"
+    
+    local ABSOLUTE_ZERO="$(abs 0)"
+    debug "abs 0 -> $ABSOLUTE_ZERO" # 0
+    is  "$ABSOLUTE_ZERO"  "0"  "op 'abs' with 0 as the arg"
+    
+    local ABS_NEG="$(abs -98.6)"
+    debug "abs -98.6 -> $ABS_NEG" # 98.6
+    is  "$ABS_NEG"  "98.6"  "op 'abs' with negative arg"
+    
+    GT31="$(gt 3 1)"
+    is "$GT31"  "'t"  "gt 3 1 -> 't"
 
+    GT12="$(gt  1 2)"
+    is "$GT12"  "nil"  "gt 1 2 -> nil"
+    
+    end
+}
 
 
 # Standard operations defined in Scheme
 
-
-
 # >op.gt,
-# abs
 # append
 # apply
 # begin
-# car
-# cdr
 # cons
 # eq?
 # expt
---
+++ tap.bash
@@ -0,0 +1,106 @@
+#!/opt/homebrew/bin/bash
+
+PLANNED=0        
+FAILS=0
+WINS=0
+        
+TEST_NUMBER=1
+        
+function techo {
+    echo "$@" >/dev/stderr
+}
+
+function ok {
+    local BOOL="$1"
+    local DESC="$2"
+    
+    local OK_P
+    if [[ "$BOOL" -eq 0 ]]; then
+        OK_P="ok"
+        ((WINS += 1))
+    else
+        OK_P="not ok"
+        ((FAILS += 1))
+    fi
+
+    if [[ "$DESC" ]]; then
+        DESC=" - $DESC"
+    fi
+    
+    techo "$OK_P ${TEST_NUMBER}${DESC}"
+    ((TEST_NUMBER += 1))
+}
+
+function is {
+    local GOT="$1"
+    local EXPECTED="$2"
+    local DESC="$3"
+    
+    if [[ "$DESC" ]]; then
+        DESC=" - $DESC"
+    fi
+    
+    local OKNOTOK DIAG
+    if [[ "$GOT" = "$EXPECTED" ]]; then
+        OKNOTOK="ok"
+        ((WINS += 1))
+    else
+        OKNOTOK="not ok"
+        ((FAILS += 1))
+        DIAG=" (got '$GOT', expected '$EXPECTED')"
+    fi
+      
+    techo "$OKNOTOK ${TEST_NUMBER}${DESC}${DIAG}"
+    ((TEST_NUMBER += 1))
+}
+
+function plan {
+    PLANNED="$1"
+    techo "1..$PLANNED"
+
+    FAILS=0
+    WINS=0    
+    TEST_NUMBER=1
+}
+
+function end {
+    local TOTAL="$((FAILS + WINS))"
+    if [[ "$TOTAL" -ne "$PLANNED" ]]; then
+        techo "warning: planned $PLANNED tests but ran $TOTAL"
+        return 1
+    elif [[ "$FAILS" -gt 0 ]]; then
+        techo "$WINS/$PLANNED tests failed"
+        return 1
+    else
+        techo "all tests succeeded"
+        return 0
+    fi
+}
+
+function tap_example_1 {
+    plan 4
+
+    true; ok $? "that which is true is true"
+    false; ok $? "polar bears are marsupials"
+    is "$((2 + 2))" 5 "2+2 = 5"
+    is "$((2 + 2))" 4 "2+2 = 4"
+    
+    end
+}
+
+function tap_example_2 {
+    plan 3
+    
+    ok true "ran test 1"
+    ok true "ran test 2"
+    
+    if false; then
+        ok "ran test 3"
+    fi
+    
+    end
+}
+
+# tap_example_1
+# techo
+# tap_example_2
